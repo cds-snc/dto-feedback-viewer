5cd86b3ffabd004c2b0df82dc90dfe61484d3c5c
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2026-02-16 12:02:14 -0500

    fix imports

--- src/main/java/ca/gc/tbs/controller/DashboardController.java
+++ src/main/java/ca/gc/tbs/controller/DashboardController.java
@@ -20,6 +20,7 @@ import javax.management.Query;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
